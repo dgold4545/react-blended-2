0d4e87388898050b7fa8fee80ce19fbaa5c10b32
Vladyslav Bielan <56whatislove@gmail.com>
2024-10-06 16:59:45 +0300

    done task 3

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,17 @@
 import { Grid, GridItem, TodoListItem } from 'components';
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, handleDelete, handleChangeEditing }) => {
   return (
     <Grid>
       {todos.map((todo, i) => (
         <GridItem key={todo.id}>
-          <TodoListItem text={todo.text} count={i + 1} />
+          <TodoListItem
+            onhandleDelete={handleDelete}
+            text={todo.text}
+            id={todo.id}
+            count={i + 1}
+            handleChangeEditing={handleChangeEditing}
+          />
         </GridItem>
       ))}
     </Grid>
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,16 +1,34 @@
 import style from './TodoListItem.module.css';
 import { Text } from 'components';
-import { RiDeleteBinLine } from 'react-icons/ri';
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const TodoListItem = ({ text, count }) => {
+export const TodoListItem = ({
+  text,
+  count,
+  onhandleDelete,
+  id,
+  handleChangeEditing,
+}) => {
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
         TODO #{count}
       </Text>
       <Text> {text} </Text>
-      <button className={style.deleteButton} type="button">
+      <button
+        onClick={() => onhandleDelete(id)}
+        className={style.deleteButton}
+        type="button"
+      >
         <RiDeleteBinLine size={24} />
+      </button>
+
+      <button
+        className={style.editButton}
+        type="button"
+        onClick={() => handleChangeEditing({ id, text })}
+      >
+        <RiEdit2Line size={24} />
       </button>
     </div>
   );
